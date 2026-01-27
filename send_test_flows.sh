17c1247b557762e3a10a2931400bfc954da66da5
Kevin <kevin@sudorandom.com>
2026-01-27 11:40:24 +0100

    chore: add send_tet_flows.sh script

---
+++ send_test_flows.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+# Default sleep duration
+SLEEP_DURATION=${1:-1}
+
+# Ensure buf and fauxrpc are available
+export PATH=$HOME/bin:$HOME/go/bin:$PATH
+
+echo "Sending flows to localhost:50051 every ${SLEEP_DURATION}s..."
+
+generate_data() {
+    while true; do
+        fauxrpc generate \
+            --schema=fauxrpc-image.binpb \
+            --target=mitmproxy.v1.ExportFlowRequest \
+            --stubs=stubs 2>/dev/null
+        sleep "$SLEEP_DURATION"
+    done
+}
+
+generate_data | buf curl \
+    --http2-prior-knowledge \
+    --schema fauxrpc-image.binpb \
+    --protocol grpc \
+    --data @- \
+    http://localhost:50051/mitmproxy.v1.Service/ExportFlow
